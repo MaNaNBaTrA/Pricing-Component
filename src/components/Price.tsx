77c6b9402d9ed0060aa3f8a4bd8c1620303ef78c
Manan Batra <mananbatra162004@gmail.com>
2024-12-06 04:38:28 +0530

    Updated Price Component

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -56,12 +56,12 @@ export default function Price() {
             const constrainedX = Math.min(Math.max(0, offsetX), boundingBox.width);
             const newPercentage = constrainedX / boundingBox.width;
             setNewPercentage(newPercentage);
-            sliderRef.current.style.background = `linear-gradient(to right, #4CAF50 ${(newPercentage) * 100}%, #ddd ${(newPercentage) * 100}%)`;
+            sliderRef.current.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${(newPercentage) * 100}%, #ddd ${(newPercentage) * 100}%)`;
             thumbRef.current.style.left = `${newPercentage * 100}%`;
         }
     };
 
-    const getCurrentViews = () => { 
+    const getCurrentViews = () => {
         const index = Math.floor(newPercentage * (pageViewOptions.length - 1));
         return pageViewOptions[index].views;
     };
@@ -78,25 +78,25 @@ export default function Price() {
             </div>
 
             <div className="flex flex-col items-center justify-center min-h-screen w-full">
-                <div className="flex flex-col items-center justify-center w-full max-w-lg p-6 bg-white dark:bg-gray-800 shadow-md rounded-lg text-black dark:text-white">
-                    <div className="flex items-center justify-center text-center">
-                        <div className="text-sm font-semibold font-manrope text-gray-400 mb-6 items-center justify-center">
+                <div className="flex flex-col items-center justify-center w-full max-w-xl  bg-white dark:bg-gray-800 shadow-md rounded-lg text-black dark:text-white ">
+                    <div className="flex items-center justify-around w-full text-center mx-6 my-10">
+                        <div className="text-base font-bold font-manrope text-gray-400 items-center justify-center tracking-widest ">
                             {getCurrentViews()} PAGEVIEWS
                         </div>
-                        <div className="flex items-center space-x-2 mb-6 justify-center">
-                            <span className="text-3xl font-bold">${getPrice()}</span>
-                            <span className="text-xl text-gray-500 dark:text-gray-400">
+                        <div className="flex items-center space-x-2 justify-center font-manrope">
+                            <span className="text-4xl font-extrabold">${getPrice()}</span>
+                            <span className="text-base text-gray-500 dark:text-gray-400 font-semibold">
                                 / {isYearlyBilling ? "year" : "month"}
                             </span>
                         </div>
                     </div>
                     <div
                         ref={sliderRef}
-                        className="relative w-[80%] h-4 mb-6 bg-gray-300 dark:bg-gray-600 rounded-full"  
+                        className="relative w-[80%] h-3 bg-gray-300 dark:bg-gray-600 rounded-full mx-6 mb-10"
                         onMouseMove={handleMouseMove}
                         onMouseUp={handleMouseUp}
                         onMouseLeave={handleMouseUp}
-                        style={{ background: '#ddd' }}  
+                        style={{ background: '#ddd' }}
                     >
                         <div
                             ref={thumbRef}
@@ -106,39 +106,65 @@ export default function Price() {
                                 left: `${newPercentage * 100}%`,
                                 top: "50%",
                                 transform: "translate(-50%, -50%)",
-                                width: "20px",
-                                height: "20px",
+                                width: "38px",
+                                height: "38px",
                                 cursor: "pointer",
                             }}
+                            className="bg-strongCyan rounded-full flex items-center justify-center"
                         >
                             <img
                                 src="/icon-slider.svg"
                                 alt="Slider Thumb"
-                                style={{
-                                    width: "100%",
-                                    height: "100%",
-                                }}
+                                className="w-1/2 h-1/2"
                             />
                         </div>
                     </div>
-                    <div className="flex items-center justify-between w-full mb-2">
-                        <span className="text-gray-600 dark:text-gray-400">Monthly Billing</span>
-                        <label className="flex items-center cursor-pointer">
-                            <input
-                                type="checkbox"
-                                checked={isYearlyBilling}
-                                onChange={toggleBilling}
-                                className="sr-only"
-                            />
-                            <div className="relative">
-                                <div className="block bg-gray-300 dark:bg-gray-600 w-14 h-8 rounded-full"></div>
-                                <div
-                                    className={`dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition ${isYearlyBilling ? "transform translate-x-6" : ""
-                                        }`}
-                                ></div>
+                    <div className="flex items-center justify-center w-full mx-6 mb-10">
+                        <span className="text-gray-400 dark:text-gray-400 text-sm font-medium">Monthly Billing</span>
+                        <div className="relative flex items-center mx-4">
+                            <label className="flex items-center cursor-pointer">
+                                <input
+                                    type="checkbox"
+                                    checked={isYearlyBilling}
+                                    onChange={toggleBilling}
+                                    className="sr-only"
+                                />
+                                <div className="relative">
+                                    <div
+                                        className={`block w-10 h-5 rounded-full transition-colors ${isYearlyBilling ? 'bg-softCyan' : 'bg-toggleBackground'} dark:${isYearlyBilling ? 'bg-blue-600' : 'bg-gray-600'}`}
+                                    ></div>
+                                    <div
+                                        className={`dot absolute top-[2.5px] left-[3px] bg-white w-[15px] h-[15px] rounded-full transition-transform ${isYearlyBilling ? 'translate-x-5' : ''}`}
+                                    ></div>
+                                </div>
+                            </label>
+                        </div>
+                        <div className="flex justify-center relative">
+                            <span className="text-gray-400 dark:text-gray-400 text-sm">Yearly Billing</span>
+                            <div className="text-lightRed text-[9px] bg-lightGrayishRed rounded-lg flex items-center justify-center p-1 font-semibold ml-2 text-nowrap absolute left-20">
+                                25% discount
                             </div>
-                        </label>
-                        <span className="text-gray-600 dark:text-gray-400">Yearly Billing (-25%)</span>
+                        </div>
+                    </div>
+                    <div className="w-full h-[1px] bg-gray-200 mb-10"></div>
+                    <div className="w-full px-6 flex mb-10 justify-between">
+                        <div>
+                            <div className="flex">
+                                <img src="/icon-check.svg" alt="" />
+                                <div>Unlimited websites</div>
+                            </div>
+                            <div className="flex">
+                                <img src="/icon-check.svg" alt="" />
+                                <div>100% data ownership</div>
+                            </div>
+                            <div className="flex">
+                                <img src="/icon-check.svg" alt="" />
+                                <div>Email reports</div>
+                            </div>
+                        </div>
+                        <div>
+                            <div>Start my trial</div>
+                        </div>
                     </div>
                 </div>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ export default {
         softCyan: "hsl(174, 77%, 80%)",          // Full Slider Bar
         strongCyan: "hsl(174, 86%, 45%)",        // Slider Background
         lightGrayishRed: "hsl(14, 92%, 95%)",    // Discount Background
-        lightRed: "hsl(15, 100%, 70%)",          // Discount Text
+        lightRed: "hsl(15, 100%, 70%)",          // Discount Text 
         paleBlue: "hsl(226, 100%, 87%)",         // CTA Text
         white: "hsl(0, 0%, 100%)",               // Pricing Component Background
         veryPaleBlue: "hsl(230, 100%, 99%)",     // Main Background
